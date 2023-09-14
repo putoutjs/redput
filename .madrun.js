e36f83cbc1e46920203be509bf59ddbdac2badb1
coderaiser <coderaiser@cloudcmd.io>
2023-09-14 15:48:18 +0300

    chore: redput: v1.10.1

---
+++ .madrun.js
@@ -0,0 +1,25 @@
+import {run} from 'madrun';
+
+const NODE_OPTIONS = `'--loader escover --no-warnings'`;
+
+const env = {
+    ESCOVER_FORMAT: 'files',
+    NODE_OPTIONS,
+};
+
+export default {
+    'lint': () => 'putout .',
+    'fresh:lint': () => run('lint', '--fresh'),
+    'lint:fresh': () => run('lint', '--fresh'),
+    'fix:lint': () => run('lint', '--fix'),
+    'test': () => `tape 'test/**/*.js' '{lib,bin}/**/*.spec.{js,mjs}'`,
+    'test:mock': async () => [env, await run('test:only')],
+    'watch:test': async () => await run('watcher', await run('test')),
+    'watch:tape': () => 'nodemon -w test -w lib --exec tape',
+    'watch:lint': async () => await run('watcher', await run('lint')),
+    'watcher': () => 'nodemon -w test -w lib -w bin --exec',
+    'coverage': async () => `c8 ${await run('test')}`,
+    'coverage:es': async () => [env, `escover ${await run('test')}`],
+    'report': () => 'c8 report --reporter=lcov',
+    'wisdom': () => run(['lint', 'coverage']),
+};
