0d58d0f07537674539fc6c7baa9c0cfde24c0d0a
coderiaser <coderaiser@cloudcmd.io>
2025-12-31 21:30:16 +0200

    chore: madrun: test: timeout

--- .madrun.js
+++ .madrun.js
@@ -1,16 +1,20 @@
-import {run} from 'madrun';
+import {run, cutEnv} from 'madrun';
+
+const env = {
+    SUPERTAPE_TIMEOUT: 2000,
+};
 
 export default {
     'lint': () => 'putout .',
     'fresh:lint': () => run('lint', '--fresh'),
     'lint:fresh': () => run('lint', '--fresh'),
     'fix:lint': () => run('lint', '--fix'),
-    'test': () => `tape 'test/**/*.js' '{lib,bin}/**/*.spec.{js,mjs}'`,
-    'watch:test': async () => await run('watcher', `"${await run('test')}"`),
+    'test': () => [env, `tape 'test/**/*.js' '{lib,bin}/**/*.spec.{js,mjs}'`],
+    'watch:test': async () => [env, await run('watcher', `"${await cutEnv('test')}"`)],
     'watch:tape': () => 'nodemon -w test -w lib --exec tape',
     'watch:lint': async () => await run('watcher', await run('lint')),
     'watcher': () => 'nodemon -w test -w lib -w bin --exec',
-    'coverage': async () => `c8 ${await run('test')}`,
+    'coverage': async () => [env, `c8 ${await cutEnv('test')}`],
     'report': () => 'c8 report --reporter=lcov',
     'wisdom': () => run(['lint', 'coverage']),
 };
